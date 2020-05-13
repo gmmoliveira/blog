83b6643cfee4a67619dc96e5c16e493e9195d43a
Guilher Oliveira <gmmoliveira1@gmail.com>
2020-05-12 21:39:42 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,5 +1,4 @@
-function loadmenu() {
-	var client = new XMLHttpRequest();
-	client.open('GET', 'menu.html');
-	document.getElementById("menu_header").innerHTML = client.responseText;
-}
+
+$(function(){
+	$("#header").load("menu.html");
+});
