a2eca0a64e0b8b7febe5f044062a2a79a80ee8aa
Guilher Oliveira <gmmoliveira1@gmail.com>
2020-05-14 21:30:20 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,4 +1,10 @@
-
 $(function(){
-	$("#header").load("menu.html");
+	var bg_height = $("#lower_top_bg_img").height();
+	var top_bg_vertical_pos = $("#lower_top_bg_img").offset().top;
+	
+	var face_height = $("#face").height();
+	var face_vertical_pos = $("#face").offset().top;
+
+	var h = face_vertical_pos - top_bg_vertical_pos - ((bg_height - face_height)/2);
+	$("#face").css({bottom: h.toString()});
 });
